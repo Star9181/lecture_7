e2ed0ceeaf71f0e7323525d3d937702b03b21771
Maulikkumar Sinha <maulikkumar.sinha@gmail.com>
2025-07-12 13:38:09 +0530

    files updated 2

--- src/index.js
+++ src/index.js
@@ -4,45 +4,44 @@ import ReactDOM from 'react-dom/client';
 // You can define a react component using 2 technique
 //1. Fucntional component
 function A(props){ //we will receive a formal argument
-  let name = "Samyak"
-  let s_class = "Nursery"
-  return <h1>Hello, {name}  {props.surname}, <B my_class={s_class}/> </h1>; //JSX
+  let A_var = "1"
+  let A_var2 = "2"
+  return <h1>{props.main_root} A {A_var} <B my_class={A_var2}/> </h1>; //JSX
 }
 
 //2. Class component
 //class Child extends Parents{}
 class B extends React.Component {
   //1. Properties/Variable
+  var3="3";
 
   constructor(){
     super();
-    this.address = "Vejalpur";
-    this.school_name="Zydus School of Excellence";
+    this.var4="4";
   }
 
   //render is menthod is compulsory
   render(){
     //To use any menber/veriable, have to use this.member
-    return <React.Fragment>
-              <span>How are you?</span><br/>
-              <span>School Name: {this.school_name}</span><br/>
-              <span>Class: {this.props.my_class}</span><br/>
-              <span>Place: {this.address}</span><br/>
-              <C>Friends:</C>
-            </React.Fragment>
+    return <span>{this.props.my_class} B {this.var3} {this.var4} <C>5</C></span>
   }
 
 }
 
 //ES6 Fat arrow function
 let C=(props)=>{
-  let name="Riya"
+  let var6="6"
   console.log("Hi", props.children);
-  return <span>{props.children} {name}</span>
+  return <span>{props.children} C {var6} <D></D></span>
+}
+
+//Function component
+function D(){
+  let var7="7"
+  return <span>D {var7}</span>
 }
 
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let sn="Sinha"
-root.render(<A surname={sn}/>);
+let main="0"
+root.render(<A main_root={main}/>);
